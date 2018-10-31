e838f6a00275c74d0318f51a7ed706f8220f147d
Biiskuit <35196333+Biiskuit@users.noreply.github.com>
2018-10-31 12:03:01 +0100

    Add files via upload

---
+++ Logging.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+namespace Logtest.Log
+{
+    class Logging
+    {
+        // Variables
+
+
+
+        public Logging(string log) {
+
+
+            // do smth
+
+            // csv helper, read whole file into an enumerable, with list
+            var csv = new CsvReader(textReader);
+            var records = csv.GetRecords<MyClass>().ToList();
+
+
+        }
+
+
+
+
+    }
+}
